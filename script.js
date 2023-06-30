3a4d74eacf35026beac7de714c902c4ac7cfd749
Sakibul-05 <110473516+Sakibul-05@users.noreply.github.com>
2023-06-30 10:31:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	const mappings = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+  };
+
+  const combinations = [];
+
+  // Helper function to generate combinations
+  function backtrack(combination, nextDigits) {
+    if (nextDigits.length === 0) {
+      combinations.push(combination);
+    } else {
+      const currentDigit = nextDigits[0];
+      const letters = mappings[currentDigit];
+
+      for (let i = 0; i < letters.length; i++) {
+        const letter = letters[i];
+        backtrack(combination + letter, nextDigits.slice(1));
+      }
+    }
+  }
+
+  if (digits.length !== 0) {
+    backtrack('', digits);
+  }
+
+  return combinations.sort();
 }
 
 module.exports = letterCombinations;
